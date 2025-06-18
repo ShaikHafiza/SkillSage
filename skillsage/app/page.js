02681550c3efbf09a436eb07b82139d8ae1e2c04
ShaikHafiza <138603502+ShaikHafiza@users.noreply.github.com>
2025-06-19 00:17:37 +0530

    folder created

--- skillsage/app/page.js
+++ skillsage/app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <h3>Hii</h3>
+      <h3> Hii  </h3>
       <br/>
-      <Button>hello</Button>
+      <button>send</button>
     </div>
   );
 }
