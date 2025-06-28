95d19e4f0c688f599d57bf6ca29d066f71a27ab6
ShaikHafiza <138603502+ShaikHafiza@users.noreply.github.com>
2025-06-28 22:27:58 +0530

    changed

--- skillsage/app/layout.js
+++ skillsage/app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
             <main className="min-h-screen">{children}</main>
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗 by Hafiza</p>
+                <p>Made with 💗&hearts; by Hafiza</p>
               </div>
             </footer>
           </ThemeProvider>
