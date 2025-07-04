5ce540648f495454afa8e1c7db691cdefb721b51
ShaikHafiza <138603502+ShaikHafiza@users.noreply.github.com>
2025-07-04 22:55:50 +0530

    Update layout.js

--- skillsage/app/layout.js
+++ skillsage/app/layout.js
@@ -1,9 +1,15 @@
 import { Inter } from "next/font/google";
+
 import "./globals.css";
+
 import { ClerkProvider } from "@clerk/nextjs";
+
 import { Toaster } from "sonner";
+
 import Header from "@/components/header";
+
 import { ThemeProvider } from "@/components/theme-provider";
+
 import { dark } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
